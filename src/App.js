556b4758609396a207a4b4a4782b5624ad60e1f0
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-05-26 14:18:39 +0300

    finish lesson 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(store => store.cash)
+  const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }
+  const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <h1>{cash}</h1>
+      <button onClick={() => addCash(Number(prompt()))}>Пополнить счёт</button>
+      <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
+    </div>    
+  )
 }
 
 export default App;
